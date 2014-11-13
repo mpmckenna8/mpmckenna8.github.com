682e7025be516bb359fc0520f18b9c04c3ca474c
Matthew McKenna <mpmckenna8@me.com>
2014-11-13 07:48:59 -0800

    Updating a few things
    mostly adding pictures. need to make sure I didn’t break my js

--- chartMap/series.js
+++ chartMap/series.js
@@ -34,19 +34,19 @@ d3.tsv('./serTots.tsv', type, function(err, series){
    console.log(series)
 
 
-x.domain(series.map(function(d){
-  return d.Team
-}))
+    x.domain(series.map(function(d){
+      return d.Team
+    }))
 
-y.domain([0,d3.max(series, function(d){
-  console.log(typeof(d.Wins));
-  return d.Wins
-})])
-
-
+    y.domain([0,d3.max(series, function(d){
+      console.log(typeof(d.Wins));
+      return d.Wins
+    })])
 
 
-  svg.append("g")
+
+
+    svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
@@ -66,30 +66,31 @@ y.domain([0,d3.max(series, function(d){
       svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -56)
-      .attr("x", -34)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Total World Series Won");
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", -56)
+          .attr("x", -34)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Total World Series Won");
 
-    svg.selectAll('.bar')
-    .data(series).enter().append('rect')
-    .attr('class', 'bar')
-    .attr('x', function(d){
-    //  console.log(d)
-      return x(d.Team)
-    })
-    .attr('width', x.rangeBand())
-    .attr('y', function(d){  return y(d.Wins);})
-    .attr('class', function(d){
-     console.log('trying to make it classy ', d)
-      return 'bar ' + d.Team + ' ' + d.id
-    })
-    .attr('height', function(d){
-      return height - y(d.Wins);
-    })
+      svg.selectAll('.bar')
+        .data(series).enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', function(d){
+        //  console.log(d)
+          return x(d.Team)
+        })
+        .attr('width', x.rangeBand())
+        .attr('y', function(d){  return y(d.Wins);})
+        .attr('class', function(d){
+         console.log('trying to make it classy ', d)
+          return 'bar ' + d.Team + ' ' + d.id
+        })
+        .attr('height', function(d){
+          return height - y(d.Wins);
+        })
+
 
 var projection = d3.geo.albers()
     .rotate([96, 0])
@@ -125,6 +126,9 @@ d3.json("./us.json", function(error, us) {
 
   d3.json('mlbSta.geojson', function(err, dat){
     console.log(dat.features)
+
+
+    /*
     svg.insert('path','.graticule')
       .datum(dat)
       .attr('d', path)
@@ -136,220 +140,228 @@ d3.json("./us.json", function(error, us) {
 
     //  .attr('fill', 'pink')
 
+*/
+
+     svg.selectAll('.stads')
+       .data(dat.features)
+       .enter()
+       .append('path')
+       .attr('d', path)
+       .attr('class', function(d,i){
+         console.log('stads classin', d);
+         return 'stads ' + d.properties.id
+       })
+       .attr('fill',function(d){
+         console.log(d.properties)
+
+        return 'blue'
+       })
+
+       svg.selectAll('.citLabs')
+       .data(dat.features)
+       .enter()
+       .append('text')
+       .text(function(d){
+        // console.log(d.properties.Team)
+         return d.properties.Team
+       })
+           .attr("transform", function(d) {
+
+                console.log(d.properties.Team)
+
+            console.log((projection(d.geometry.coordinates)))
+            var endCo = projection(d.geometry.coordinates)[0]
+
+            console.log(endCo)
+
+             return 'translate(' + projection(d.geometry.coordinates) + ')'
+             //'translate(' + path(d.geometry[1]) +','path(d.geometry[0]) + ')'
+
+             //"translate(" + path.centroid(d) + ")"; })
+      })
+      .attr('class', 'citLabs')
+      .attr("dy", ".41em")
+      .attr('dx', '4em')
+      .text(function(d){
+        //console.log(d.properties.Team)
+        return d.properties.Team
+      })
+      .attr('d',path)
+      .attr('text-anchor','end')
+      .attr('font-size', 13)
 
 
-   svg.selectAll('.stads')
-     .data(dat.features)
-     .enter()
-     .append('path')
-     .attr('d', path)
-     .attr('class', function(d,i){
-       console.log('stads classin', d);
-       return 'stads ' + d.properties.id
-     })
-     .attr('fill',function(d){
-       console.log(d.properties)
+       d3.selectAll('.citLabs')
+         .attr('stroke', function(d){
+           console.log('color of text is white', d)
+           return 'white'
+         })
+         .attr('class', function (d){
+           return 'citLabs ' + d.properties.id
 
-      return 'blue'
-     })
+         })
+         .attr('fill', 'red')
+         .attr('transform', function(d){
+          // console.log((this))
 
-     svg.selectAll('.citLabs')
-     .data(dat.features)
-     .enter()
-     .append('text')
-     .text(function(d){
-      // console.log(d.properties.Team)
-       return d.properties.Team
-     })
-         .attr("transform", function(d) {
+           var coords = projection(d.geometry.coordinates)
+           console.log(d.properties.Team)
+           if(d.properties.Team == "San Francisco Giants"){
+             console.log('trans giants')
+          //   console.log(coords)
+             coords[0] += -30;
+             coords[1] += -15
+           }
+           else if(d.properties.Team == "Los Angeles Dodgers"){
+             console.log(coords)
+             coords[0] += 10;
+             coords[1] += -20
+           }
+           else if(d.properties.Team == "San Diego Padres"){
+             console.log('trans giants')
+            // console.log(coords)
+             coords[0] += -20;
+             coords[1] += 10
+           }
+           else if(d.properties.Team == "Arizona Diamondbacks"){
+             console.log('trans giants')
+             console.log(coords)
+             coords[0] += 30;
+             coords[1] += -5
+           }
+           else if(d.properties.Team == "Minnesota Twins"){
+             console.log('trans giants')
+             console.log(coords)
+             coords[0] += -5;
+             coords[1] += -10
+           }
 
-              console.log(d.properties.Team)
+          else if(d.properties.Team == "Toronto Blue Jays"){
+            console.log('trans giants')
+            console.log(coords)
+            coords[0] += -10;
+            coords[1] += -15
+          }
+          else if(d.properties.Team == "Boston Red Sox"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += 25;
+            coords[1] += -15
+          }
+          else if(d.properties.Team == "New York Yankees"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += 45;
+            coords[1] += -15
+          }
+          else if(d.properties.Team == "New York Mets"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += 20;
+            coords[1] += 0
+          }
+          else if(d.properties.Team == "Philadelphia Phillies"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += 65;
+            coords[1] += 0
+          }
+          else if(d.properties.Team == "Baltimore Orioles"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += 45;
+            coords[1] += 0
+          }
+          else if(d.properties.Team == "Milwaukee Brewers"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += -45;
+            coords[1] += -10
+          }
+          else if(d.properties.Team == "Detroit Tigers"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += -15;
+            coords[1] += -10
+          }
+          else if(d.properties.Team == "Colorado Rockies"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += -25;
+            coords[1] += -6
+          }
+          else if(d.properties.Team == "Kansas City Royals"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += -5;
+            coords[1] += -9
+          }
+          else if(d.properties.Team == "Washington Nationals"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += 70;
+            coords[1] += 10
+          }
+          else if(d.properties.Team == "Chicago White Sox"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += -54;
+            coords[1] += -9
+          }
+          else if(d.properties.Team == "Chicago Cubs"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += -54;
+            coords[1] += 7
+          }
+          else if(d.properties.Team == "Pittsburgh Pirates"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += -34;
+            coords[1] += 8
+          }
+          else if(d.properties.Team == "St. Louis Cardinals"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += 14;
+            coords[1] += 8
+          }
+          else if(d.properties.Team == "Tampa Bay Devil Rays"){
+            console.log('trans bosox')
+            console.log(coords)
+            coords[0] += 0;
+            coords[1] += -8
+          }
 
-          console.log((projection(d.geometry.coordinates)))
-          var endCo = projection(d.geometry.coordinates)[0]
 
-          console.log(endCo)
+      return 'translate(' + coords + ')'
 
-           return 'translate(' + projection(d.geometry.coordinates) + ')'
-           //'translate(' + path(d.geometry[1]) +','path(d.geometry[0]) + ')'
-
-           //"translate(" + path.centroid(d) + ")"; })
     })
-    .attr('class', 'citLabs')
-    .attr("dy", ".41em")
-    .attr('dx', '4em')
-    .text(function(d){
-      //console.log(d.properties.Team)
-      return d.properties.Team
-    })
-    .attr('d',path)
-    .attr('text-anchor','end')
-    .attr('font-size', 13)
 
 
-     d3.selectAll('.citLabs')
-       .attr('stroke', function(d){
-         console.log('color of text is white', d)
-         return 'white'
-       })
-       .attr('class', function (d){
-         return 'citLabs ' + d.properties.id
-
-       })
-       .attr('fill', 'red')
-       .attr('transform', function(d){
-        // console.log((this))
-
-         var coords = projection(d.geometry.coordinates)
-         console.log(d.properties.Team)
-         if(d.properties.Team == "San Francisco Giants"){
-           console.log('trans giants')
-        //   console.log(coords)
-           coords[0] += -30;
-           coords[1] += -15
-         }
-         else if(d.properties.Team == "Los Angeles Dodgers"){
-           console.log(coords)
-           coords[0] += 10;
-           coords[1] += -20
-         }
-         else if(d.properties.Team == "San Diego Padres"){
-           console.log('trans giants')
-          // console.log(coords)
-           coords[0] += -20;
-           coords[1] += 10
-         }
-         else if(d.properties.Team == "Arizona Diamondbacks"){
-           console.log('trans giants')
-           console.log(coords)
-           coords[0] += 30;
-           coords[1] += -5
-         }
-         else if(d.properties.Team == "Minnesota Twins"){
-           console.log('trans giants')
-           console.log(coords)
-           coords[0] += -5;
-           coords[1] += -10
-         }
-
-        else if(d.properties.Team == "Toronto Blue Jays"){
-          console.log('trans giants')
-          console.log(coords)
-          coords[0] += -10;
-          coords[1] += -15
-        }
-        else if(d.properties.Team == "Boston Red Sox"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += 25;
-          coords[1] += -15
-        }
-        else if(d.properties.Team == "New York Yankees"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += 45;
-          coords[1] += -15
-        }
-        else if(d.properties.Team == "New York Mets"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += 20;
-          coords[1] += 0
-        }
-        else if(d.properties.Team == "Philadelphia Phillies"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += 65;
-          coords[1] += 0
-        }
-        else if(d.properties.Team == "Baltimore Orioles"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += 45;
-          coords[1] += 0
-        }
-        else if(d.properties.Team == "Milwaukee Brewers"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += -45;
-          coords[1] += -10
-        }
-        else if(d.properties.Team == "Detroit Tigers"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += -15;
-          coords[1] += -10
-        }
-        else if(d.properties.Team == "Colorado Rockies"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += -25;
-          coords[1] += -6
-        }
-        else if(d.properties.Team == "Kansas City Royals"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += -5;
-          coords[1] += -9
-        }
-        else if(d.properties.Team == "Washington Nationals"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += 70;
-          coords[1] += 10
-        }
-        else if(d.properties.Team == "Chicago White Sox"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += -54;
-          coords[1] += -9
-        }
-        else if(d.properties.Team == "Chicago Cubs"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += -54;
-          coords[1] += 7
-        }
-        else if(d.properties.Team == "Pittsburgh Pirates"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += -34;
-          coords[1] += 8
-        }
-        else if(d.properties.Team == "St. Louis Cardinals"){
-          console.log('trans bosox')
-          console.log(coords)
-          coords[0] += 14;
-          coords[1] += 8
-        }
-
-
-return 'translate(' + coords + ')'
-
-       })
-
-d3.select('#info')
-.style('opacity', .4)
+  d3.select('#info')
+    .style('opacity', .4)
 
 
   d3.selectAll('.bar')
-  .on('mouseover', function(d){
-    console.log( d)
-    var infor = d;
-    var idStr = '.' + d.id;
+    .on('mouseover', function(d){
 
-    d3.selectAll(idStr)
-      .attr('stroke', 'red')
-      .attr('fill', 'white')
-      .attr('font-size', 16)
+      console.log( d)
+      var infor = d;
+      var idStr = '.' + d.id;
+
+      d3.selectAll(idStr)
+        .attr('stroke', 'red')
+        .attr('fill', 'white')
+        .attr('font-size', 16)
 
 
-  d3.selectAll(idStr +'.stads')
-    .attr('fill',function(d){
-    //  console.log(d.properties)
+      d3.selectAll(idStr +'.stads')
+        .attr('fill',function(d){
+        //  console.log(d.properties)
 
-     return 'yellow'
-    })
+         return 'yellow'
+        })
 
 
 
@@ -357,41 +369,41 @@ d3.select('#info')
 
 
     d3.select('#info')
-      .style('opacity', 1)
-     .html(function(d){
-    //   console.log('in lab ')
+        .style('opacity', 1)
+       .html(function(d){
+      //   console.log('in lab ')
 
-       return '<p>' +infor.Team + '</br>Total wins: ' +infor.Wins +'</br> Total losses: '+ infor.Losses + '</br>Total Appearances: '+infor.Series_appearances +'</p>'
-     })
+         return '<p>' +infor.Team + '</br>Total wins: ' +infor.Wins +'</br> Total losses: '+ infor.Losses + '</br>Total Appearances: '+infor.Series_appearances +'</p>'
+       })
 
 
       d3.selectAll('.bar')
         .attr('stroke', 'red')
         .attr('fill', 'white')
 
-  })
-  .on('mouseout', function(d){
-    var idStr = '.' + d.id;
+    })
+    .on('mouseout', function(d){
+      var idStr = '.' + d.id;
 
-    d3.selectAll(idStr)
-      .attr('stroke', 'white')
-      .attr('fill', function(d){
-      //  console.log('outco', d)
-      return 'blue'
-      })
-      .attr('font-size', 13)
-
-
-    d3.selectAll('.stads')
-        .attr('fill',function(d){
-        //  console.log(d.properties)
-
-         return 'blue'
+      d3.selectAll(idStr)
+        .attr('stroke', 'white')
+        .attr('fill', function(d){
+        //  console.log('outco', d)
+        return 'blue'
         })
+        .attr('font-size', 13)
 
-  d3.selectAll('#info p').remove()
 
-  })
+      d3.selectAll('.stads')
+          .attr('fill',function(d){
+          //  console.log(d.properties)
+
+           return 'blue'
+          })
+
+    d3.selectAll('#info p').remove()
+
+    })
 
 
   d3.selectAll('.citLabs')
