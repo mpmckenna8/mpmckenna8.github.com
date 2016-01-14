dee12e49aa6e0360bd3c4f53244669e49ea443f0
mpmckenna8 <mpmckenna8@me.com>
2016-01-14 13:50:26 -0800

    added jquery ui which i didn't really use but did make some meaningful changes in my js and some content.

--- indescript.js
+++ indescript.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
                   $('.tabs li').click(function(){
                     if (!$(this).hasClass('selected')){
                         $('.tabs li').removeClass('selected');
@@ -11,5 +12,52 @@ $(document).ready(function(){
                             $('.content').fadeIn('slow');
                                                             });
                                       });
-                  
-                  });
+
+
+                    if( $(window).width() < 640){
+                      var navers = $(".tabs").children('li');
+                      console.log(navers.children('li'));
+
+                      //var navsp = ["All"];
+
+
+
+                      $(".tabs").css("display", "none");
+
+                      $(".nav").append("<select class='bleep'> <option>Navigate      ╲╱</option> </select>");
+
+
+                      for(i=0; i < navers.length; i++){
+
+                          console.log($(navers[i]).text())
+
+                          $('.bleep').append("<option value=" + (navers[i]).id  + ">" + $(navers[i]).text() + "</option>")
+
+                        //  var htm = (navers[0].children[i]);
+                        //  console.log(htm.id);
+
+                      }
+
+                     $(".bleep").change(function(){
+                        console.log($(this).val());
+                        var displ = $(this).val();
+
+                        if(displ == 'all'){
+                          $(".page").css("display", "block")
+                        }
+                        else{
+                          $('.content').fadeOut('slow', function(){
+                              $('div .page').css('display','none');
+                              $('.page#'+displ).css('display','block');
+                              $('.content').fadeIn('slow');
+                                                              });
+                                        }
+                                        });
+
+
+                      }
+
+
+                    })
+
+                  ;
